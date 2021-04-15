105a1b0ba48296cc26a9cdecfcb5a8e4a687808a
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-04-15 21:14:25 +0900

    init project

---
+++ src/main/java/me/moon/Mtube/MtubeApplication.java
@@ -0,0 +1,13 @@
+package me.moon.Mtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtubeApplication.class, args);
+	}
+
+}
