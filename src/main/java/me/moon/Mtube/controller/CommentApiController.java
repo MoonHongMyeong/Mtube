9ddaafb04f28a2906621a0b57ec1413c30a8192f
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-04-26 17:32:22 +0900

    Fix : MRQ_022 : 빼먹은 GetMapping 추가

--- src/main/java/me/moon/Mtube/controller/CommentApiController.java
+++ src/main/java/me/moon/Mtube/controller/CommentApiController.java
@@ -3,6 +3,7 @@ package me.moon.Mtube.controller;
 import lombok.RequiredArgsConstructor;
 import me.moon.Mtube.dto.comment.CommentResponseDto;
 import me.moon.Mtube.service.CommentService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,14 @@ public class CommentApiController {
     private final CommentService commentService;
 
     //댓글 리스트 조회
+    @GetMapping
     public List<CommentResponseDto> getComment(@PathVariable("postId") Long postId){
         return commentService.getComment(postId);
     }
+    //댓글 등록
+
+    //댓글 수정
+
+    //댓글 삭제
+
 }
