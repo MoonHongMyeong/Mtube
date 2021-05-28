7a35c7d840322fb40ee69f470057e794ae798311
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-05-28 20:23:09 +0900

    Refactor : PostController, Service [#23]

--- src/main/java/me/moon/Mtube/controller/PostApiController.java
+++ src/main/java/me/moon/Mtube/controller/PostApiController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import me.moon.Mtube.dto.post.PostResponseDto;
 import me.moon.Mtube.dto.post.PostSaveRequestDto;
 import me.moon.Mtube.dto.post.PostUpdateRequestDto;
+import me.moon.Mtube.dto.user.UserResponseDto;
 import me.moon.Mtube.service.PostService;
 import me.moon.Mtube.service.SessionLoginUser;
 import me.moon.Mtube.util.Message;
@@ -38,14 +39,14 @@ public class PostApiController {
     //포스트 조회
     @GetMapping("/api/v1/video/{postId}")
     public PostResponseDto getPost(@PathVariable("postId") Long postId){
-        String userEmail=loginUser.getCurrentUser();
-        return postService.getPost(userEmail, postId);
+        UserResponseDto userDto=loginUser.getCurrentUser();
+        return postService.getPost(userDto, postId);
     }
 
     @PutMapping("/api/v1/video/{postId}/recordEnd")
     public ResponseEntity recordEnd(@PathVariable("postId") Long postId){
-        String userEmail=loginUser.getCurrentUser();
-        postService.recordEnd(userEmail, postId);
+        UserResponseDto userDto=loginUser.getCurrentUser();
+        postService.recordEnd(userDto, postId);
         return new ResponseEntity(new Message("기록종료"), HttpStatus.OK);
     }
     //채널에 등록된 포스트 조회
@@ -56,29 +57,29 @@ public class PostApiController {
     //포스트 등록
     @PostMapping("/api/v1/channel/{channelId}/video")
     public ResponseEntity addPost(@PathVariable("channelId") Long channelId, @RequestBody PostSaveRequestDto saveRequestDto){
-        String userEmail = loginUser.getCurrentUser();
-        postService.addPost(userEmail, channelId, saveRequestDto);
+        UserResponseDto userDto = loginUser.getCurrentUser();
+        postService.addPost(userDto, channelId, saveRequestDto);
         return new ResponseEntity(new Message("post add success!"), HttpStatus.CREATED);
     }
     //임시 포스트 등록
     @PostMapping("/api/v1/channel/{channelId}/video/temp")
     public ResponseEntity addTempPost(@PathVariable("channelId") Long channelId ,@RequestBody PostSaveRequestDto saveRequestDto){
-        String userEmail = loginUser.getCurrentUser();
-        postService.addTempPost(userEmail, channelId, saveRequestDto);
+        UserResponseDto userDto = loginUser.getCurrentUser();
+        postService.addTempPost(userDto, channelId, saveRequestDto);
         return new ResponseEntity(new Message("temp post add success"), HttpStatus.CREATED);
     }
     //포스트 수정
     @PutMapping("/api/v1/channel/{channelId}/video/{postId}")
     public ResponseEntity updatePost(@PathVariable("channelId") Long channelId, @PathVariable("postId") Long postId, @RequestBody PostUpdateRequestDto updateRequestDto){
-        String userEmail = loginUser.getCurrentUser();
-        postService.updatePost(userEmail, channelId, postId, updateRequestDto);
+        UserResponseDto userDto = loginUser.getCurrentUser();
+        postService.updatePost(userDto, channelId, postId, updateRequestDto);
         return new ResponseEntity(new Message("post update success!"),HttpStatus.OK);
     }
     //포스트 삭제
     @DeleteMapping("/api/v1/channel/{channelId}/video/{postId}")
     public ResponseEntity deletePost(@PathVariable("channelId") Long channelId, @PathVariable("postId")Long postId){
-        String userEmail = loginUser.getCurrentUser();
-        postService.deletePost(userEmail, channelId, postId);
+        UserResponseDto userDto = loginUser.getCurrentUser();
+        postService.deletePost(userDto, channelId, postId);
         return new ResponseEntity(new Message("post delete success!"), HttpStatus.OK);
     }
 }
