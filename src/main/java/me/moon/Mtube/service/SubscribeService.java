bbd22a5e3ebc56bd6564df2af9dea41994a4ce8c
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-05-30 20:32:25 +0900

    Refactor : SubscribeController, Service[#23]

--- src/main/java/me/moon/Mtube/service/SubscribeService.java
+++ src/main/java/me/moon/Mtube/service/SubscribeService.java
@@ -1,6 +1,7 @@
 package me.moon.Mtube.service;
 
 import lombok.RequiredArgsConstructor;
+import me.moon.Mtube.dto.user.UserResponseDto;
 import me.moon.Mtube.exception.UnsuitableUserException;
 import me.moon.Mtube.mapper.SubscribeMapper;
 import me.moon.Mtube.mapper.UserMapper;
@@ -13,23 +14,21 @@ public class SubscribeService {
     private final SubscribeMapper subscribeMapper;
     private final UserMapper userMapper;
 
-    public void subscribe(String userEmail, Long channelId) {
-        Long userId = userMapper.findUserByEmail(userEmail).getId();
-        if(toExistSubscribe(userId, channelId)){
+    public void subscribe(UserResponseDto userDto, Long channelId) {
+        if(toExistSubscribe(userDto.getId(), channelId)){
             throw new UnsuitableUserException("이미 구독 중 입니다.");
         }
-        subscribeMapper.subscribe(userId, channelId);
+        subscribeMapper.subscribe(userDto.getId(), channelId);
     }
 
     private boolean toExistSubscribe(Long userId, Long channelId) {
         return subscribeMapper.toExistSubscribe(userId, channelId);
     }
 
-    public void cancelSubscribe(String userEmail, Long channelId) {
-        Long userId = userMapper.findUserByEmail(userEmail).getId();
-        if(!toExistSubscribe(userId, channelId)){
+    public void cancelSubscribe(UserResponseDto userDto, Long channelId) {
+        if(!toExistSubscribe(userDto.getId(), channelId)){
             throw new UnsuitableUserException("구독 중이 아닙니다. \n 잘못 된 요청입니다.");
         }
-        subscribeMapper.cancelSubscribe(userId, channelId);
+        subscribeMapper.cancelSubscribe(userDto.getId(), channelId);
     }
 }
