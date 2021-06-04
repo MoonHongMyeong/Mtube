c8cba602987788b88851a4b7ea7617dcd615423c
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-06-04 20:34:22 +0900

    Add : User SignUp, updateUserInfo, changePassword, withdrawal test[#6]

---
+++ src/test/java/me/moon/Mtube/controller/UserApiControllerTest.java
@@ -0,0 +1,115 @@
+package me.moon.Mtube.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.moon.Mtube.dto.user.*;
+import me.moon.Mtube.mapper.UserMapper;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class UserApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @Order(1)
+    @DisplayName("회원 가입에 성공한다.")
+    public void signUp() throws Exception {
+        String url = "http://localhost:"+port+"/api/v1/user";
+        UserSaveRequestDto saveRequestDto = UserSaveRequestDto.builder()
+                .email("test@test.com")
+                .name("testUser")
+                .password("12345")
+                .picture("/test.png")
+                .build();
+        mvc.perform(post(url)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(saveRequestDto)))
+                .andExpect(status().isCreated());
+    }
+    @Test
+    @Order(2)
+    @DisplayName("회원 정보 수정에 성공한다.")
+    public void updateUser() throws Exception {
+        String exceptName = "updateUser";
+        String exceptPicture = "/update.png";
+
+        LoginUserDto loginUserDto = userMapper.findUserByEmail("test@test.com");
+
+        String url = "http://localhost:"+port+"/api/v1/user/"+loginUserDto.getId();
+
+        UserUpdateRequestDto updateRequestDto = UserUpdateRequestDto.builder()
+                .name(exceptName)
+                .picture(exceptPicture)
+                .build();
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(updateRequestDto)))
+                .andExpect(status().isOk());
+
+        UserResponseDto userResponseDto = userMapper.getUser(loginUserDto.getId());
+        assertThat(userResponseDto.getName().equals(exceptName));
+        assertThat(userResponseDto.getPicture().equals(exceptPicture));
+
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("비밀번호 변경에 성공한다.")
+    public void changePassword() throws Exception {
+        LoginUserDto loginUserDto = userMapper.findUserByEmail("test@test.com");
+
+        MockHttpSession session = new MockHttpSession();
+
+        String url = "http://localhost:"+port+"/api/v1/user/"+loginUserDto.getId()+"/password";
+
+        session.setAttribute("USER", userMapper.getUser(loginUserDto.getId()));
+
+        UserChangePasswordDto userChangePasswordDto = UserChangePasswordDto.builder()
+                .password("12345")
+                .newPassword("54321")
+                .build();
+
+        mvc.perform(put(url)
+                .session(session)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(userChangePasswordDto)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("회원탈퇴에 성공한다.")
+    public void withdrawal() throws Exception {
+        LoginUserDto loginUserDto = userMapper.findUserByEmail("test@test.com");
+
+        String url = "http://localhost:"+port+"/api/v1/user/"+loginUserDto.getId();
+
+        mvc.perform(delete(url)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
