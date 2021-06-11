11a1b645dd4d44a32b88640c992c5a2627a56155
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-06-11 19:38:10 +0900

    Add : 회원의 플레이리스트 목록 조회 [#2]

--- src/main/java/me/moon/Mtube/dto/playlist/UserPlaylistResponseDto.java
+++ src/main/java/me/moon/Mtube/dto/playlist/UserPlaylistResponseDto.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class UserPlaylistResponseDto {
+    private String id;
     private String name;
     private String playlistName;
 }
