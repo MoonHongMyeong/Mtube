4e21010199fc74d51ad492c061505a42663f2bdc
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-07-03 19:02:11 +0900

    Test : 포스트 좋아요 테스트

---
+++ src/test/java/me/moon/Mtube/controller/LikeApiControllerTest.java
@@ -0,0 +1,59 @@
+package me.moon.Mtube.controller;
+
+import me.moon.Mtube.dto.post.PostResponseDto;
+import me.moon.Mtube.dto.user.LoginUserDto;
+import me.moon.Mtube.mapper.CommentMapper;
+import me.moon.Mtube.mapper.PostMapper;
+import me.moon.Mtube.mapper.UserMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LikeApiControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private PostMapper postMapper;
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @Test
+    @DisplayName("포스트의 좋아요에 성공한다.")
+    public void likePost() throws Exception{
+        LoginUserDto userDto = userMapper.findUserByEmail("test@test.com");
+        MockHttpSession session = new MockHttpSession();
+
+        List<PostResponseDto> postlist = postMapper.getPostList();
+
+        Long postId = postlist.get(0).getId();
+
+        session.setAttribute("USER", userDto);
+
+        String url = "http://localhost:"+port+"/api/v1/video/"+postId+"/like";
+
+        mvc.perform(post(url).session(session)).andExpect(status().isOk());
+    }
+}
