52d26d8ca0cc3cf255b6270d01b6a15d23835d60
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-07-10 21:00:54 +0900

    Test : 댓글의 싫어요 테스트

--- src/test/java/me/moon/Mtube/controller/LikeApiControllerTest.java
+++ src/test/java/me/moon/Mtube/controller/LikeApiControllerTest.java
@@ -129,4 +129,23 @@ public class LikeApiControllerTest {
 
         mvc.perform(post(url).session(session)).andExpect(status().isCreated());
     }
+
+    @Test
+    @DisplayName("댓글의 싫어요에 성공한다.")
+    public void dislikeComment() throws Exception{
+        LoginUserDto userDto = userMapper.findUserByEmail("test@test.com");
+        MockHttpSession session = new MockHttpSession();
+
+        List<PostResponseDto> postlist = postMapper.getPostList();
+        Long postId = postlist.get(0).getId();
+
+        List<CommentResponseDto> commentList = commentMapper.getCommentList(postId);
+        Long commentId = commentList.get(0).getId();
+
+        session.setAttribute("USER", userDto);
+
+        String url = "http://localhost:"+port+"/api/v1/video/"+postId+"/comment/"+commentId+"/dislike";
+
+        mvc.perform(post(url).session(session)).andExpect(status().isCreated());
+    }
 }
